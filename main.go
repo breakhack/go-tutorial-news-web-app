fe1d303c974faaae66c6a4a3f69d71143542f18f
Anastas Mironov <thebreakhack@gmail.com>
2020-05-20 15:21:48 +0900

    change default port to 3001

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
 	"os"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("no .env file found")
+	}
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hello World!!!</h1>"))
 }
 
-func main() {
+func getPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
+	return port
+}
 
+func main() {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":"+port, mux)
+	http.ListenAndServe(":"+getPort(), mux)
 }
